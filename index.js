9264e893d5b230e0ee6eb6613840a6864e2f8a5f
Alex Gochenour <alexgochenour@gmail.com>
2022-03-10 23:13:40 -0500

    feat: Now takes an argument from user. Also added links and a .gitignore.

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 #!/usr/bin/env node
-console.log("Alex Gochenour");
+const [,, ...args] = process.argv;
+
+console.clear();
+const name = (args.length > 0)
+  ? args
+  : "World";
+
+console.log(`
+  Hello ${name}!
+  
+  I'm Alex Gochenour, and I am an award-winning* JavaScript engineer and founder of Woobler's House. 
+  I love baking, running, and all things Chinese.
+  
+  🔗 Linkedin: https://www.linkedin.com/in/alex-gochenour
+  🐈 Github: https://github.com/chunyukuo88
+  
+  *I was named "Person of the Year" by Time Magazine in 2006.
+`);
