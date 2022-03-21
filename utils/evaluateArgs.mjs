624b1a1687cb39289bfdec49e2c11300e2568a3c
Alex Gochenour <alexgochenour@gmail.com>
2022-03-21 00:05:32 -0400

    Boxen and improved colors.

--- utils/evaluateArgs.mjs
+++ utils/evaluateArgs.mjs
@@ -1,14 +1,12 @@
 import { printAboutText } from './about.mjs';
 import { printHelpText } from './help.mjs';
+import boxen from "boxen";
 
 export function evaluateArgs(argsArray){
   const validPair = getFirstValidFlagFunctionPair(argsArray);
-  if (validPair) {
-    const correspondingFunction = validPair[1];
-    correspondingFunction();
-  } else {
-    printNoArgsFound()
-  }
+  if (!validPair) return printNoArgsFound();
+  const correspondingFunction = validPair[1];
+  correspondingFunction();
 }
 
 function getFirstValidFlagFunctionPair(argsArray){
@@ -24,11 +22,11 @@ function getFirstValidPair(userArgsOnly, flagFunctionPairs){
 }
 
 function printNoArgsFound(){
-  const availableFlags = Object.values(flagsAndAliases).map(pair => ('\n\t'+pair[0]));
-  console.log(`
-    No flags or inputs detected. Try running this CLI with the --help flag for detailed information or try one of the following valid flags and aliases:
-    ${availableFlags}
-  `);
+  const availableFlags = Object.values(flagsAndAliases).map(pair => ('\n      '+pair[0]));
+  console.log(boxen(`
+    No flags or inputs detected. 
+    Try running this CLI with the --help flag for detailed information or try one of the following valid flags and aliases:  \n${availableFlags}
+  `));
 }
 
 const flagsAndAliases = {
